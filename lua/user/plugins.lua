7fb12e77f47223541f67f1c29529d1a2319a5a7b
Michael McHolm <michael.mcholm@triumphtech.com>
2023-02-28 13:51:07 -0500

    add markdown preview

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -95,6 +95,7 @@ return packer.startup(function(use)
     commit = "8e763332b7bf7b3a426fd8707b7f5aa85823a5ac",
   }
   use { "hashivim/vim-terraform" }
+  use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 
   -- Git
   use { "lewis6991/gitsigns.nvim", commit = "f98c85e7c3d65a51f45863a34feb4849c82f240f" }
